e10569dbab85d2806d02228932f0b6c446fb1587
Siphon880gh <weng.f.fung@gmail.com>
2025-04-01 23:32:11 -0700

    Added php version

--- php/index.php
+++ php/index.php
@@ -1,3 +1,12 @@
+<?php
+    $filepath = $_GET['filepath'] ?? "";
+    $imagepath = $_GET['imagepath'] ?? "";
+
+    if(strlen($filepath)===0) {
+        echo "Make sure to have filepath= in url search params.";
+        die();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,17 +15,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes</title>
+    <script>
+        const filepath = "<?php echo $filepath; ?>";
+        const imagepath = "<?php echo $imagepath; ?>";
+    </script>
+
+    <link href="//cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <style>
         img {
-            width: 100%;
+            display: block;
+            margin: 0 auto;
         }
-        
-        @media (min-width: 768px) {
-            img {
-                width: 50%;
-            }
+        body {
+            text-align: center;
         }
-        
         #toc-toggler {
             cursor: pointer;
             position: fixed;
@@ -79,6 +93,32 @@
         .h6::before {
             content: "◦\00a0";
         }
+        p {
+            margin-bottom: 1.5em; /* This creates double line spacing between paragraphs */
+        }
+        img {
+            max-height: 600px;
+        }
+    </style>
+
+    <style>
+    ul {
+        list-style-type: none;
+        padding-left: 1.25rem;
+    }
+
+    ol {
+        list-style-type: decimal;
+        padding-left: 1.25rem;
+        display: table;
+        margin: 0 auto;
+    }
+    ul > li::before {
+        content: "•\00a0";
+    }
+    table {
+        margin: 0 auto;
+    }
     </style>
 </head>
 
@@ -87,7 +127,13 @@
         <div id="mobile-tap" onclick="event.target.classList.toggle('active')">📖</div>
         <div id="toc"></div>
     </div>
-    <div class="container"></div>
+
+    <h1 class="text-4xl font-bold text-center"><?php 
+        $filename = substr($filepath, strpos($filepath, '/') + 1);
+        echo substr($filename, 0, strrpos($filename, '.'));
+    ?></h1>
+
+    <div class="container text-2xl"></div>
 
     <script src="https://cdn.jsdelivr.net/npm/markdown-it@13.0.1/dist/markdown-it.min.js"></script>
     <script>
@@ -115,12 +161,20 @@
                 toc.appendChild(div);
             });
         }
-        fetch("_Biz - Scrum.md", {
+        fetch(filepath, {
                 cache: "no-cache"
             }).then(response => response.text())
             .then(myMarkdown => {
-                var md = window.markdownit();
+                var md = window.markdownit({
+                    html: true,
+                    linkify: true
+                });
+                
+                
                 var result = md.render(myMarkdown);
+                result = result.replace(/!\[\[(.*?)\]\]/g, function(match, filename) {
+                    return `<img src="${imagepath}/${filename}" alt="${filename}">`;
+                });
                 document.querySelector(".container").innerHTML = result;
                 htmlTableOfContents(document.querySelector(".container"))
             });
---
+++ php/md/android-c7/Enter numbers during call.md
@@ -0,0 +1,22 @@
+
+**Sometimes you'll need to enter extra numbers after making a call.**  
+This happens when you're prompted by an automated voice system—like when using a prepaid calling card to call Hong Kong, or when contacting a government line that asks you to enter your card number.
+
+**How to Enter Numbers During a Call:**  
+Once your call connects and you hear the automated voice, you won’t be able to type numbers right away.  
+![[Pasted image 20250401011147.png]]
+
+To make this easier, **turn on Speaker Mode**—this lets you hear the instructions clearly while keeping the phone in front of you, so you can see and use the keypad without holding the phone to your ear. Press the speaker icon (See highlighted in gray):
+![[Pasted image 20250401011419.png]]
+
+
+Open the number pad by lightly tapping the **keypad icon** on the screen. It's the icon with 10 dots:
+![[Pasted image 20250401011630.png]]
+
+After that, there are numbers appearing on your screen. By tapping these numbers, you can enter the additional numbers when prompted by the automated system's computer voice.
+![[Pasted image 20250401011728.png]]
+
+---
+
+Let's walk through a hypothetical situation. You called a government agency's phone number. Their automated system asked to press "2" for Chinese. You pressed "2". Then it asks to press "1" to check your card's balance. You press 1. The phone's display shows a history of what buttons have been pressed:
+![[Pasted image 20250401012019.png]]
